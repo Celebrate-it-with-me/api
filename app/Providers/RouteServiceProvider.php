3e97519676ea981e3f82cfdf097bf1c0de55111e
Henry Carmenate <henrycarmenateg@gmail.com>
2025-01-19 23:22:45 -0500

    Few changes in the api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,14 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api/v1/')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('api')
+                ->prefix('api/v1/app')
+                ->group(base_path('routes/api/app.php'));
+
+            Route::middleware('api')
+                ->prefix('api/v1/admin')
+                ->group(base_path('routes/api/admin.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
---
+++ database/seeders/RolesAndPermissionSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Creating Permissions
+        $permissions = [
+          'manage',
+          'app'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create([
+               'name' => $permission,
+            ]);
+        }
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo(['manage']);
+
+        $appUserRole = Role::create(['name' => 'appUser']);
+        $appUserRole->givePermissionTo(['app']);
+
+        $user = User::query()->firstOrCreate(
+            [
+                'email' => 'henrycarmenateg@gmail.com'
+            ],
+            [
+                'name' => 'Henry Carmenate',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $user->assignRole('admin');
+
+        $this->command->info('Roles and Permissions seeded successfully!');
+    }
+}
