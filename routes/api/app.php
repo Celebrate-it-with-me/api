f481e388661b4ae7c3989357319461e093771557
Henry Carmenate <henrycarmenateg@gmail.com>
2025-01-24 16:01:53 -0500

    Move logout route inside auth middleware and add logging.
    Relocated the logout route to be protected by the "auth:sanctum" middleware for better security. Added logging of the user data during logout for debugging and monitoring purposes.

--- routes/api/app.php
+++ routes/api/app.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\AuthenticationController;
 
 Route::post('register', [AuthenticationController::class, 'appRegister']);
 Route::post('login', [AuthenticationController::class, 'appLogin']);
-Route::post('logout', [AuthenticationController::class, 'appLogout']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('event', [EventsController::class, 'store']);
     Route::get('event', [EventsController::class, 'index']);
+    
+    Route::post('logout', [AuthenticationController::class, 'appLogout']);
 });
 
