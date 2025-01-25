7e2771ab1fc273dbefa703cd1fea491d2b75ff2c
Henry Carmenate <henrycarmenateg@gmail.com>
2025-01-24 23:26:09 -0500

    Working in logins logs

---
+++ database/migrations/2025_01_25_041235_create_user_login_sessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_login_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('login_time');
+            $table->string('ip_address')->nullable();
+            $table->string('browser')->nullable();
+            $table->string('device')->nullable();
+            $table->string('location')->nullable();
+            $table->dateTime('logout_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_login_sessions');
+    }
+};
