75ed3009d66ffd7f5e1379d5f7ec36f3d4291e81
Henry Carmenate <henrycarmenateg@gmail.com>
2025-02-05 20:58:06 -0500

    Add SuggestedMusicConfig feature with model, migration, and APIs
    Implemented a new SuggestedMusicConfig feature, which includes a model, migration, resource, services, and controller. Integrated RESTful API routes for managing configurations and established associations with the Events model. This enables creating, updating, retrieving, and deleting suggested music configurations tied to specific events.

---
+++ app/Http/Controllers/AppControllers/SuggestedMusicConfigController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\AppControllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\app\StoreSuggestedMusicConfigRequest;
+use App\Http\Requests\app\UpdateSuggestedMusicConfigRequest;
+use App\Http\Resources\AppResources\SuggestedMusicConfigResource;
+use App\Http\Services\AppServices\SuggestedMusicConfigServices;
+use App\Models\Events;
+use App\Models\SuggestedMusicConfig;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Throwable;
+
+class SuggestedMusicConfigController extends Controller
+{
+    
+    private SuggestedMusicConfigServices $suggestedMusicConfigServices;
+    
+    public function __construct(SuggestedMusicConfigServices $suggestedMusicConfigServices)
+    {
+        $this->suggestedMusicConfigServices = $suggestedMusicConfigServices;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Events $event): JsonResponse|AnonymousResourceCollection
+    {
+        try {
+            return SuggestedMusicConfigResource::collection($this->suggestedMusicConfigServices->getSuggestedMusicConfig($event));
+        } catch (Throwable $th) {
+            return response()->json(['message' => $th->getMessage(), 'data' => []], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSuggestedMusicConfigRequest $request, Events $event): JsonResponse|SuggestedMusicConfigResource
+    {
+        try {
+            return SuggestedMusicConfigResource::make($this->suggestedMusicConfigServices->create($event));
+        } catch (Throwable $th) {
+            return response()->json(['message' => $th->getMessage(), 'data' => []], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SuggestedMusicConfig $suggestedMusicConfig): SuggestedMusicConfigResource|JsonResponse
+    {
+        try {
+            return SuggestedMusicConfigResource::make($suggestedMusicConfig);
+        } catch (Throwable $th) {
+            return response()->json(['message' => $th->getMessage(), 'data' => []], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSuggestedMusicConfigRequest $request, SuggestedMusicConfig $suggestedMusicConfig): SuggestedMusicConfigResource|JsonResponse
+    {
+        try {
+            return SuggestedMusicConfigResource::make($this->suggestedMusicConfigServices->update($suggestedMusicConfig));
+        } catch (Throwable $th) {
+            return response()->json(['message' => $th->getMessage(), 'data' => []], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SuggestedMusicConfig $suggestedMusicConfig): SuggestedMusicConfigResource|JsonResponse
+    {
+        try {
+            return SuggestedMusicConfigResource::make($this->suggestedMusicConfigServices->destroy($suggestedMusicConfig));
+        } catch (Throwable $th) {
+            return response()->json(['message' => $th->getMessage(), 'data' => []], 500);
+        }
+    }
+}
---
+++ app/Policies/SuggestedMusicConfigPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\SuggestedMusicConfig;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class SuggestedMusicConfigPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, SuggestedMusicConfig $suggestedMusicConfig): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, SuggestedMusicConfig $suggestedMusicConfig): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, SuggestedMusicConfig $suggestedMusicConfig): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, SuggestedMusicConfig $suggestedMusicConfig): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, SuggestedMusicConfig $suggestedMusicConfig): bool
+    {
+        return false;
+    }
+}
