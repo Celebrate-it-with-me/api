79691d8314ebef7396086f480a4da7caca9d0e03
Henry Carmenate <henrycarmenateg@gmail.com>
2025-03-21 01:07:35 -0400

    Add GuestCompanionObserver to handle model events
    Introduced a new observer for the GuestCompanion model to manage lifecycle events. The observer updates the main guest's companion quantity on deletion and has placeholders for other events. Registered the observer in the EventServiceProvider for automatic monitoring.

---
+++ app/Observers/GuestCompanionObserver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\GuestCompanion;
+use Illuminate\Support\Facades\Log;
+
+class GuestCompanionObserver
+{
+    /**
+     * Handle the GuestCompanion "created" event.
+     */
+    public function created(GuestCompanion $guestCompanion): void
+    {
+        //
+    }
+    
+    /**
+     * Handle the GuestCompanion "updated" event.
+     */
+    public function updated(GuestCompanion $guestCompanion): void
+    {
+        //
+    }
+    
+    /**
+     * Handle the GuestCompanion "deleted" event.
+     * Updating companions qty if one companion is deleted.
+     */
+    public function deleted(GuestCompanion $guestCompanion): void
+    {
+        $countCompanions = GuestCompanion::query()
+            ->where('main_guest_id', $guestCompanion->main_guest_id)
+            ->count();
+        
+        $guestCompanion->mainGuest->companion_qty = $countCompanions;
+        $guestCompanion->mainGuest->save();
+    }
+    
+    /**
+     * Handle the GuestCompanion "restored" event.
+     */
+    public function restored(GuestCompanion $guestCompanion): void
+    {
+        //
+    }
+    
+    /**
+     * Handle the GuestCompanion "force deleted" event.
+     */
+    public function forceDeleted(GuestCompanion $guestCompanion): void
+    {
+        //
+    }
+}
